b9ab96f00748f26a72fa4b6e32cac1334bf10d33
Clebson Ramos <clebsonramoss@gmail.com>
2025-01-31 16:37:07 -0300

    Correção das nomenclaturas e identificações do GPIO 12.

--- Tarefa_Atividade_01.c
+++ Tarefa_Atividade_01.c
@@ -3,11 +3,11 @@
 #include "hardware/timer.h"
 
 #define LED_VERMELHO 13
-#define LED_AZUL 12
+#define LED_AMARELO 12 // Azul no BitDogLab
 #define LED_VERDE 11
 
 //-----VARIÁVEIS GLOBAIS-----
-char status_do_semaforo[3] = {'R', 'B', 'G'};
+char status_do_semaforo[3] = {'R', 'Y', 'G'};
 int marcador_status_semaforo = 0;
 
 //-----PROTÓTIPOS-----
@@ -23,17 +23,17 @@ bool repeating_timer_callback(struct repeating_timer *ponteiro){
     switch(status_do_semaforo[marcador_status_semaforo]){
         case 'R': // Sinal vermelho
             gpio_put(LED_VERMELHO, true);
-            gpio_put(LED_AZUL, false);
+            gpio_put(LED_AMARELO, false);
             gpio_put(LED_VERDE, false);
             break;
         case 'B': // Sinal amarelo (azul no BitDogLab)
             gpio_put(LED_VERMELHO, false);
-            gpio_put(LED_AZUL, true);
+            gpio_put(LED_AMARELO, true);
             gpio_put(LED_VERDE, false);
             break;
         case 'G': // Sinal verde
             gpio_put(LED_VERMELHO, false);
-            gpio_put(LED_AZUL, false);
+            gpio_put(LED_AMARELO, false);
             gpio_put(LED_VERDE, true);
             break;
     }
@@ -57,7 +57,7 @@ int main(void){
     while(true){
         if(status_do_semaforo[marcador_status_semaforo] == 'R'){
             printf("SINAL VERMELHO - Aguarde.\n");
-        }else if(status_do_semaforo[marcador_status_semaforo] == 'B'){
+        }else if(status_do_semaforo[marcador_status_semaforo] == 'Y'){
             printf("SINAL AMARELO - Atencao.\n");
         }else if(status_do_semaforo[marcador_status_semaforo] == 'G'){
             printf("SINAL VERDE - Prossiga.\n");
@@ -70,9 +70,9 @@ int main(void){
 //-----FUNÇÕES COMPLEMENTARES-----
 void inicializacao_dos_pinos(void){
     gpio_init(LED_VERMELHO);
-    gpio_init(LED_AZUL);
+    gpio_init(LED_AMARELO);
     gpio_init(LED_VERDE);
     gpio_set_dir(LED_VERMELHO, GPIO_OUT);
-    gpio_set_dir(LED_AZUL, GPIO_OUT);
+    gpio_set_dir(LED_AMARELO, GPIO_OUT);
     gpio_set_dir(LED_VERDE, GPIO_OUT);
 }
